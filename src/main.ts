1a6226a0a9ae2750cb4217d3219fb4c290ce592d
Sandeep Ramgolam <sandeep@ramgolam.com>
2023-04-17 14:34:51 +0400

    added router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router';
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes, // short for `routes: routes`
+  })
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
